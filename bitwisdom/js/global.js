b9925822bde3a58496d25ee464ca0ff2d5fb3ef7
Lubaba Nuzhat Tasneem <lubaba070@gmail.com>
2022-06-23 11:42:47 -0700

    Adding custom bitwisdom theme

---
+++ bitwisdom/js/global.js
@@ -0,0 +1,20 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bitwisdomCountNodes = {
+    attach: function(context) {
+      console.log('global file loaded');
+
+      var total = 0;
+
+      $(context).find('article.node').each(function() {
+        total++;
+      });
+
+      console.log('Found ' + total + ' nodes.');
+
+    }
+  };
+
+ })(jQuery, Drupal);
